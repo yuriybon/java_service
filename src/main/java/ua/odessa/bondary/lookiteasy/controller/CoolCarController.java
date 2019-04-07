8685783950b913f69f3e3003d71a6ddc118bb591
bondary <DCTghjcnj_01>
2019-04-07 13:56:25 +0200

    Example to drop image by REST

--- src/main/java/ua/odessa/bondary/lookiteasy/controller/CoolCarController.java
+++ src/main/java/ua/odessa/bondary/lookiteasy/controller/CoolCarController.java
@@ -1,11 +1,16 @@
 package ua.odessa.bondary.lookiteasy.controller;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ua.odessa.bondary.lookiteasy.domain.Car;
 import ua.odessa.bondary.lookiteasy.repository.CarRepository;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -30,5 +35,16 @@ class CoolCarController {
                 !car.getName().equals("Triumph Stag") &&
                 !car.getName().equals("Ford Pinto") &&
                 !car.getName().equals("Yugo GV");
+    }
+
+    @GetMapping(
+            value = "/get-image-with-media-type
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody
+    byte[] getImageWithMediaType() throws IOException {
+        InputStream in = getClass().getResourceAsStream("/ua/bondary/produceimage/image.jpg");
+
+        return IOUtils.toByteArray(in);
     }
 }
